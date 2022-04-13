b8cbb9ed0652a4530fccdc89757df21da3eadcb6
w1res <103469384+w1res@users.noreply.github.com>
2022-04-13 10:08:20 -0300

    Create ui.py

---
+++ src/interface/ui.py
@@ -0,0 +1,12 @@
+from rich import print
+
+
+def ascii() -> str:
+    """Print ASCII on screen"""
+
+    file_path: str = "src/interface/ascci.txt"
+
+    with open(file_path) as ascii_file:
+        content_ascii = ascii_file.read()
+
+        print(f"[magenta][b]{content_ascii}[/][/]")

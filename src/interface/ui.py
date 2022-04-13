8f3efe8b35cb805a718d7f946bfb0bcaadf700d4
w1res <103469384+w1res@users.noreply.github.com>
2022-04-13 11:28:39 -0300

    Update ui.py

--- src/interface/ui.py
+++ src/interface/ui.py
@@ -4,7 +4,7 @@ from rich import print
 def ascii() -> str:
     """Print ASCII on screen"""
 
-    file_path: str = "src/interface/ascci.txt"
+    file_path: str = "src/interface/ascii.txt"
 
     with open(file_path) as ascii_file:
         content_ascii = ascii_file.read()
